3a4d36adda775e8203ac855f5bbbca90f981a62f
sussegadin <gilmarmorinigogm@gmail.com>
2026-09-19 10:19:19 -0300

    Atualiza roteamento e estilos do site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,10 @@ import Editor from './pages/Editor';
 import Sponsors from './pages/Sponsors';
 import Louvores from './pages/Louvores';
 import LouvoresAdmin from './pages/LouvoresAdmin';
+import AnnouncementsAdmin from './pages/AnnouncementsAdmin';
+import SiteContentAdmin from './pages/SiteContentAdmin';
+import TestimonialsAdmin from './pages/TestimonialsAdmin';
 import AdminLogin from './pages/AdminLogin';
 import NotFound from './pages/NotFound';
-function Router(){return <Switch><Route path="/" component={Home}/><Route path="/blog" component={Blog}/><Route path="/post/:slug" component={Post}/><Route path="/sponsors" component={Sponsors}/><Route path="/louvores" component={Louvores}/><Route path="/admin/login" component={AdminLogin}/><Route path="/admin/editor" component={Editor}/><Route path="/admin/louvores" component={LouvoresAdmin}/><Route component={NotFound}/></Switch>}
+function Router(){return <Switch><Route path="/" component={Home}/><Route path="/blog" component={Blog}/><Route path="/post/:slug" component={Post}/><Route path="/sponsors" component={Sponsors}/><Route path="/louvores" component={Louvores}/><Route path="/admin/login" component={AdminLogin}/><Route path="/admin/editor" component={Editor}/><Route path="/admin/louvores" component={LouvoresAdmin}/><Route path="/admin/avisos" component={AnnouncementsAdmin}/><Route path="/admin/textos" component={SiteContentAdmin}/><Route path="/admin/testemunhos" component={TestimonialsAdmin}/><Route component={NotFound}/></Switch>}
 export default function App(){return <ErrorBoundary><ThemeProvider defaultTheme="dark"><TooltipProvider><Toaster/><RadioProvider><SiteShell><Router/></SiteShell></RadioProvider></TooltipProvider></ThemeProvider></ErrorBoundary>}
